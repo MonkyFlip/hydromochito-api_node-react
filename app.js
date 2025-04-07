490b3c2451884b7aa9734209764cf6737a674f46
MonkyFlip <155855166+MonkyFlip@users.noreply.github.com>
2025-04-07 17:52:24 -0600

    third commit: menu.ejs para ver rutas

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const routes = require('./routes'); // Tus rutas definidas
+const path = require('path');
 
+// Middleware
 app.use(bodyParser.json());
+
+// Configurar EJS como motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware para configurar las cabeceras CORS
 app.use((req, res, next) => {
@@ -11,6 +17,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
+});
+
+// Ruta para mostrar el menú de inicio
+app.get('/', (req, res) => {
+  res.render('menu'); // Renderiza la vista "menu.ejs"
 });
 
 // Rutas de la API
